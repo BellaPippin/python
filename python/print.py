119ee193623802257545cc41a51f2835af2ff931
Maria Cosenza <127864622+BellaPippin@users.noreply.github.com>
2024-01-23 11:41:17 -0600

    Adding Python folder

---
+++ python/print.py
@@ -0,0 +1,13 @@
+print("Bel")
+print ("This is an argument"," So is this one"," And this one")
+print ("\n\n\n\n")
+
+print ("Knock knock.")
+print ("Who is it?")
+print ("Hello")
+print ("Hello who?")
+print ("Hello World!")
+print ("\n\n\n\n")
+
+
+print ('Knock knock. \nWho is it? \n Hello. \n Hello who? \n Hello World!')
