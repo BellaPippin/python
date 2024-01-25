9e306b6cfdf5a6975cb94ed5fd77c479442e1f8d
Maria Cosenza <127864622+BellaPippin@users.noreply.github.com>
2024-01-25 11:40:19 -0600

    A program that calculates the area of a rectangle.

---
+++ python/my_math.py
@@ -0,0 +1,17 @@
+# Welcome
+print("\n\n\n\n\n~Welcome to my math program!~\n\n")
+
+# We state the math problem
+print("Problem: A rectangle with a width of 2 and a length of 14 has what area?\n")
+print("Thinking...\n\n")
+
+# We assign the variables
+width = 2
+length = 14
+area = width * length
+
+# We print the answer! Yahoo.
+print("Here is my answer:\n\n")
+print("A rectangle with a width of " + str(width) + " and a length of " + str(length) + " has an area of: " + str(area) + "!\n\n\n\n")
+
+
