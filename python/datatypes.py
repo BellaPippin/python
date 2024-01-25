a3d2a6d6c88b83e4a16f3dc4745b85b911050e40
Maria Cosenza <127864622+BellaPippin@users.noreply.github.com>
2024-01-25 11:44:28 -0600

    Playing with datatypes

---
+++ python/datatypes.py
@@ -0,0 +1,50 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = "What did I do"
+print("Literal:", x)
+
+# Integer
+y = 42
+print("Integer:", y)
+
+# Float
+z = 50.75
+print("Float:", z)
+
+# String
+name = "Maria C."
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
+
+print ("\n\n\n\n\n")
+print ("*******************")
+print ("\n\n\n\n\n")
+
+#Literal
+
+my_variable = "Whoooo are you. Who who, who who"
+print(my_variable)
+
+# Now a string
+
+string_variable = "It's kinda like the last one"
+print(string_variable)
+
+# Aaaand an integer
+
+integer_variable = (10*10)
+print(integer_variable)
+
+# Finally, a Boolean
+
+boolean_variable = True
+print (boolean_variable)
+
+print ("\n\n\n\n\n")
+print ("*******************")
+print ("\n\n\n\n\n")
+
