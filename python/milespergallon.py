7dc424afd7438055fe15c3fb28e29eb0dab70a8b
Maria Cosenza <127864622+BellaPippin@users.noreply.github.com>
2024-01-30 11:50:32 -0600

    Miles per Gallon Comments

---
+++ python/milespergallon.py
@@ -0,0 +1,13 @@
+# This program calculates how many miles per gallon a car gets.
+
+# First we set the tank size.
+tank_size = 15
+
+# Then we set the distance.
+distance = 600
+
+# Now the math.
+mpg = distance / tank_size
+
+# Finally, we display the result.
+print("Miles per gallon:", mpg)
