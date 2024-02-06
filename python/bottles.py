91f63b319d11c289e235b5c92993099071a6cdb0
Maria Cosenza <127864622+BellaPippin@users.noreply.github.com>
2024-02-06 12:02:04 -0600

    bottles on the wall

--- python/bottles.py
+++ python/bottles.py
@@ -3,13 +3,19 @@ print ("\n\n\n")
 
 count = 100
 while count > 0:
-    print(f"{count} bottles of beer on the wall")
-    print(f"{count} bottles of beer")
-    print("Take one down, pass it around")
-    print(f"{count - 1} bottles of beer\n\n")
+    if count == 1:
+        print(f"{count} bottle of beer on the wall")
+        print(f"{count} bottle of beer")
+        print("Take one down, pass it around")
+        print(f"{count - 1} bottles of beer\n\n")
+    else:
+        print(f"{count} bottles of beer on the wall")
+        print(f"{count} bottles of beer")
+        print("Take one down, pass it around")
+        print(f"{count - 1} bottles of beer\n\n")
     count -= 1  # Decrease
-if count == 1:
-    print(f"{count} bottle of beer on the wall")
-    print(f"{count} bottle of beer")
-    print("Take one down, pass it around")
-    print(f"{count - 1} bottles of beer\n\n")
+
+print("No more bottles of beer on the wall, no more bottles of beer.")
+print("Go to the store and buy some more, 99 bottles of beer on the wall.")
+    
+
