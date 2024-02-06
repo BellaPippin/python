0919b236d4e943f8e19f1270f3dbe48c1d2f5287
Maria Cosenza <127864622+BellaPippin@users.noreply.github.com>
2024-02-06 15:47:06 -0600

    Divisible by 7

---
+++ python/divisible_by_7.py
@@ -0,0 +1,8 @@
+# This program checks if numbers are divisible by 7. 
+# Print some n's first..
+print ("\n\n\n")
+
+# Run a loop to check numbers. 
+for number in range(1, 300):
+    if number % 7 == 0: # Check if reminder of division is 0
+        print (number) # Print the number if the previous statement returns true.
