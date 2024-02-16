21bd18417df3bfdd39d65be55e266b4f55954a42
BellaPippin <mbcosenza@hotmail.com>
2024-02-15 19:21:17 -0600

    Count even numbers in nested lists of diff types

--- python/even_numbers_mixed.py
+++ python/even_numbers_mixed.py
@@ -4,14 +4,12 @@ print ("\n\n")
 # Initialize list
 my_list = [["Apple", 3, 6, "String"],["IDK", 10, True, "Meh"],[20, 43, 42, "Yahoo"]]
 counter = 0
-is_integer = ""
 
 # Find even numbers and add it to counter
 for item in my_list:
     for i in item:
-        is_integer = isinstance(i, int)
-        if is_integer:
-            if i % 2 == 0:
+        if isinstance(i, int):  # Check if the item is an integer
+            if i % 2 == 0:  # Check if the integer is even
                 counter += 1
 
 print (counter)
