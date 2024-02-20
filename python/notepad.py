df7b323380c9e2ac000591f5fd411fdbf3392968
Maria Cosenza <mbcosenza@hotmail.com>
2024-02-20 11:57:00 -0600

    new

---
+++ python/notepad.py
@@ -0,0 +1,18 @@
+TAX = .05 # tax is a constant, I capitalize constants
+def main(): #function
+    sales_total = sales() # assigning result of a function to a variable
+    sales_tax(sales_total) # passing a variable to a function 
+    print(f"sales_total: {sales_total}")
+
+def sales_tax(sales_total): # variable becomes parameter
+    # duck throwing football for passing
+    sales_total = 1000.00
+    tax_amt = sales_total * TAX
+    print(tax_amt)
+
+def sales():
+    my_sales = float(input("What is the price:  "))
+    return my_sales
+
+
+main()
