9dc84f544a4d01cf1d05e2b9e33419986ecfb0db
BellaPippin <mbcosenza@hotmail.com>
2024-02-21 14:35:14 -0600

    madlibs

--- python/madlibs.py
+++ python/madlibs.py
@@ -1,30 +1,30 @@
 # Hit me baby one more time!
 
-def madlibs(noun, verb, color, noun_2, verb_2):
-    print: ("\n\n")
-    print: (f"Oh {noun}, {noun}")
-    print: (f"How was I supposed to {verb}")
-    print: (f"That something wasn't {color}")
+def madlibs(noun, verb, color, noun_2, verb_2, noun_3, verb_3, verb_4):
+    print ("\n\n")
+    print (f"Oh {noun}, {noun}")
+    print (f"How was I supposed to {verb}")
+    print (f"That something wasn't {color}")
 
-    print: ("\n\n")
-    print: (f"Oh {noun_2}, {noun_2}")
-    print: (f"I shouldn't have {verb_2}")
-    print: ("And now you're out of sight, yeahh")
+    print ("\n\n")
+    print (f"Oh {noun_2}, {noun_2}")
+    print (f"I shouldn't have {verb_2}")
+    print ("And now you're out of sight, yeahh")
 
 
-    print: ("\n\n")
-    print: ("Show me how you want it to be")
-    print: ("Tell me baby")
-    print: ("'Cause I need to know now, oh, becauseeee")
+    print ("\n\n")
+    print ("Show me how you want it to be")
+    print ("Tell me baby")
+    print ("'Cause I need to know now, oh, becauseeee")
 
-    print: ("\n\n")
-    print: ("My loneliness is killing me")
-    print: ("(And IiiiIIi...)")
-    print: ("I must confess I still believe")
-    print: ("(I still believe)")
-    print: ("When I'm not with you I lose my mind")
-    print: ("Give me a siiiign")
-    print: ("Hit me baby one more time!")
+    print ("\n\n")
+    print (f"My {noun_3} is killing me")
+    print ("(And IiiiIIi...)")
+    print (f"I must confess I still {verb_3}")
+    print (f"(I still {verb_3})")
+    print ("When I'm not with you I lose my mind")
+    print ("Give me a siiiign")
+    print (f"{verb_4} me baby one more time!")
 
 
 noun = input("Enter a noun here:  ")
@@ -32,7 +32,10 @@ verb = input("Enter some verb:  ")
 color = input ("Type in a color:  ")
 noun_2 = input("Enter some other noun:  ")
 verb_2 = input("Enter some other verb in past tense:  ")
+noun_3 = input("Enter yet another noun:  ")
+verb_3 = input("Enter some other verb:  ")
+verb_4 = input("Enter one last verb:  ")
 
 
-madlibs(noun, verb, color, noun_2, verb_2)
+madlibs(noun, verb, color, noun_2, verb_2, noun_3, verb_3, verb_4)
 
