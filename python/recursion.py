8870e9ec859e1b78d41538c88a04f1003d78280d
Maria Cosenza <mbcosenza@hotmail.com>
2024-02-27 10:56:59 -0600

    Factorials

---
+++ python/recursion.py
@@ -0,0 +1,20 @@
+# This program calculates factorials
+print ("\n\n")
+
+# this function calculates the factorial
+def factorial(number):
+    if number == 1 or number == 0:
+        return 1
+    else:
+        return number * factorial(number - 1)
+
+# 'main' runs the program
+def main():
+    print ("Hello! Let's calculate some factorials!")
+    number = int(input("Please enter a non-negative integer:  "))
+    print (f"Your factorial number is {factorial(number)}\n\n")
+
+
+
+
+main()
