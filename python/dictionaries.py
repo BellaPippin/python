89956060be74c279ca30a83ec51eb218e2bcefca
Maria Cosenza <mbcosenza@hotmail.com>
2024-02-29 11:15:29 -0600

    Dictionaries

---
+++ python/dictionaries.py
@@ -0,0 +1,39 @@
+# Creating and using dictionaries
+# This program translates words into the NATO alphabet.
+print ("\n\n")
+
+# Initialize dictionary
+nato_dictionary = {
+    "A": "Alpha",
+    "B": "Bravo",
+    "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo",
+    "F": "Foxtrot",
+    "G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliette",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "X-Ray",
+    "Y": "Yankee",
+    "Z": "Zulu" }
+
+# This function takes the user input, converts it to uppercase and converts it into the NATO alphabet.
+def spell_word():
+    user_word = input("Write a word: ")
+    user_word.upper()
+
+    
