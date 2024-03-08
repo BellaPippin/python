042cdc4c8f4d803b63d56f63dc00a6e0d2d27527
BellaPippin <mbcosenza@hotmail.com>
2024-03-08 11:32:32 -0600

    modules

---
+++ python/modules/main.py
@@ -0,0 +1,13 @@
+from math_operations import calculator
+
+result = calculator.add(5, 3)
+print("Addition result:", result)
+
+result = calculator.subtract(10, 4)
+print("Subtraction result:", result)
+
+from math_operations import geometry
+
+print("Rectangle Area:", geometry.rectangle(5, 10))
+print("Triangle Area:", geometry.triangle(4, 6))
+print("Circle Area:", geometry.circle(3))
---
+++ python/modules/math_operations/geometry.py
@@ -0,0 +1,9 @@
+def rectangle (length, width):
+    return length * width
+
+def triangle (base, height):
+    return 0.5 * base * height
+
+def circle (radius):
+    pi = 3.14
+    return pi * radius ** 2
