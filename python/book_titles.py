e6b93da27dc463435e0c25f53b327651fe3f09be
Maria Cosenza <mbcosenza@hotmail.com>
2024-03-14 11:39:26 -0500

    Book Titles

---
+++ python/book_titles.py
@@ -0,0 +1,21 @@
+# This program takes book titles and sorts them alphabetically.
+print("\n\n")
+
+def main():
+    book_list = []
+
+    for i in range (10):     # Ask for 10 titles, conver to lower case for comparison, append to new list
+        book = input("Enter a book title! ")
+        book_lower = book.lower()
+        book_list.append(book_lower)
+
+
+    book_list_sorted = sorted(book_list) # Sort list
+    
+    print ("\nHere are your books in alphabetical order!\n")
+    for book in book_list_sorted: # Re-capitalize each book title
+        print(book.title())
+
+
+
+main()
