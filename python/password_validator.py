d9c93b898db1cafd01f4423f46b5d0503e2c2341
Maria Cosenza <mbcosenza@hotmail.com>
2024-03-19 12:01:44 -0500

    Password Validator

--- python/password_validator.py
+++ python/password_validator.py
@@ -6,36 +6,25 @@ def main():
     valid = False
 
     while not valid:
-            valid = True   # This will be False if any requirements are not met.
-            print("""Password Requirements:\n
-            Between 8 to 20 characters long.\n
-            Contains at least one uppercase letter.\n
-            Contains at least one lowercase letter.\n
-            Includes at least one number.\n
-            Includes at least one symbol from the set: !@#$%&*\n""")
-             
-            password = input("Please enter a password that meets the criteria:")
-    
             try:
+                valid = True   # This will be False if any requirements are not met.
+                print("""Password Requirements:\n
+                Between 8 to 20 characters long.\n
+                Contains at least one uppercase letter.\n
+                Contains at least one lowercase letter.\n
+                Includes at least one number.\n
+                Includes at least one symbol from the set: !@#$%&*\n""")
+                
+                password = input("Please enter a password that meets the criteria:")
+    
+            
                 if 7 < len(password) < 21:
-                     continue
+                     valid = True
                 else:
                      valid = False
                      print ("Password is not the right length!")
 
-                for ch in password:
-                     if ch.isupper():
-                        continue
-                     else:
-                        valid = False
-                        print ("There has to be an uppercase letter!")
-
-                for ch in password:
-                     if ch.islower():
-                        continue
-                     else:
-                        valid = False
-                        print ("There has to be a lowercase letter!")
+                
 
                 
                 
