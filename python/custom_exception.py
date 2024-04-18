e1a037d27feff92fb02e3f56520a32020743648e
Maria Cosenza <mbcosenza@hotmail.com>
2024-04-18 11:42:23 -0500

    Custom Exception

--- python/custom_exception.py
+++ python/custom_exception.py
@@ -4,20 +4,18 @@ print ("\n\n")
 class  NotNumericError(ValueError):
     def __init__(self, message="You did not enter a number. Please enter a number!"):
         self.message = message
-        super().__init__(self.message)
-
+        
     def __str__(self):
-        return f'{self.message}'
+        return f"{self.message}"
 
 
 def main():
     try:
         # Get input from user
-        value = int(input("Please enter a value from 0 to 9: "))
+        value = input("Please enter a value from 0 to 9: ")
 
-
-    except ValueError:
-        raise NotNumericError()
+        if not value.isnumeric():
+            raise NotNumericError()
     
     except Exception as e:
         print(f"The exception is: {e}")
