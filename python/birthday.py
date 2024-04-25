3677680b5c06be15ec2a4bddf41c3613a39b829b
Maria Cosenza <mbcosenza@hotmail.com>
2024-04-25 10:59:43 -0500

    Datetime Module

---
+++ python/birthday.py
@@ -0,0 +1,41 @@
+# This program calculates a person's age in: Years, months, weeks, days
+# 30.41 days in a month
+# 7 days in a week 
+
+# Import datetime module
+from datetime import datetime
+
+def main():
+   
+    print("\n\n")
+    try:
+        today = datetime.today()
+
+        birth_year = int(input("What year were you born?  "))
+        month = int(input("What month were you born (as a number. May would be 5)  "))
+        day = int(input("What day of the month were you born?  "))
+        
+        #Make a date object with user's input and display it
+        birthday = datetime(birth_year, month, day)
+        birthday_output = birthday.strftime("%Y-%m-%d")
+        print(f"Ok. You were born on {birthday_output}.\n") 
+
+        # Do math based on the day difference
+        delta = today - birthday
+        delta_years = delta.days // 365.2425
+        delta_months = delta.days // 12
+        delta_weeks = delta.days // 7
+        delta_days = delta.days
+       
+        # Display results
+        print (f'You are {delta_years} years old.')
+        print (f"That is {delta_months} months!")
+        print (f"That is {delta_weeks} weeks!")
+        print (f"That is {delta_days} days!")
+
+       
+      
+    except Exception as e:
+        print(f"ooooops!!!:  {e}") 
+        main()
+main()
