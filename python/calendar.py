6baf0e00e2e57ba9a1ed90dd571d91de118af3f5
Maria Cosenza <mbcosenza@hotmail.com>
2024-04-25 11:46:25 -0500

    Calendar Module

---
+++ python/calendar.py
@@ -0,0 +1,26 @@
+# Getting and displaying calendars from the calendar module
+print ("\n\n")
+
+# Import module
+import calendar, datetime
+
+def main():
+
+    try:
+        
+        # Ask user for their birth month
+        month = int(input("What's your birth month (as a number)?" ))
+
+        
+
+    except Exception as e:
+        print (f"Oopsie happened: {e}")
+
+
+
+
+
+
+
+
+main()
